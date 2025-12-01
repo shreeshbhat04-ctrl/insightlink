8d11e1123d72f858a56752f6ae6ca1e77c8a8824
shreeshbhat04-ctrl <shreeshbhat04@gmail.com>
2025-12-01 18:16:52 +0530

    final push

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
-import './Auth.css'; // << same CSS
+import './AuthStyle.css'; // << same CSS
 
 function LoginPage() {
   const [formData, setFormData] = useState({
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
-import './Auth.css'; // << use this CSS
+import './AuthStyle.css'; // << use this CSS
 
 function RegisterPage() {
   const [formData, setFormData] = useState({
